f528f38e853a8c253bd3d8e4d31209fab2bd13ef
Gamal Shaban <gemy21ce@gmail.com>
2014-01-24 22:05:55 +0200

    categories and subscategories view for super judge.

---
+++ application/models/category.php
@@ -0,0 +1,1 @@
+<?phpclass Category extends DataMapper {    var $table = 'category';    var $model = 'category';        var $has_many = array('subcategory');        function __construct() {        parent::__construct();    }   }?>
---
+++ application/models/subcategory.php
@@ -0,0 +1,1 @@
+<?phpclass Subcategory extends DataMapper {    var $table = 'sub_category';    var $model = 'subcategory';        var $has_one = array('category');        function __construct() {        parent::__construct();    }   }?>
