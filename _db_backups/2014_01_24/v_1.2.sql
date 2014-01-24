84ca6542d307731c2f5ab7a1b3b281248fb491aa
Gamal Shaban <gemy21ce@gmail.com>
2014-01-24 16:45:02 +0200

    login and logout

--- _db_backups/2014_01_24/v_1.2.sql
+++ _db_backups/2014_01_24/v_1.2.sql
@@ -2,3 +2,10 @@ INSERT INTO `user_role` (`role`)VALUES (  'admin');
 INSERT INTO `user_role` (`role`) VALUES ('super_admin');
 INSERT INTO `user_role` (`role`) VALUES ('judge');
 INSERT INTO `user_role` (`role`) VALUES ('super_judge');
+
+
+ALTER TABLE  `users` DROP FOREIGN KEY  `users_ibfk_1` ;
+ALTER TABLE  `users` CHANGE  `user_role`  `role_id` INT( 11 ) NOT NULL;
+ALTER TABLE  `users` ADD FOREIGN KEY (  `role_id` ) REFERENCES  `user_role` (
+`id`
+) ON DELETE CASCADE ON UPDATE CASCADE ;
