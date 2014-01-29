1bbf1751fe2bb430272cc6095545c4f85f0ddc98
Gamal Shaban <gemy21ce@gmail.com>
2014-01-29 22:25:25 +0200

    admin user ui.

--- application/views/backend/includes/footer.php
+++ application/views/backend/includes/footer.php
@@ -1,3 +1,1 @@
-<Div class="footer">Powered By : <a href="#">ICS</a></Div>
-</body>
-</html>
+<div class="Footer">    <div class="footer-company-logo"><a href="#"><img src="<?php echo base_url(); ?>assets/frontend/images/misr-el-kheir.jpg" /></a>        <a href="#"><img src="<?php echo base_url(); ?>assets/frontend/images/intel.jpg"/></a>    </div>    <div class="footer-text">        Copyright Â© Intel ISEF Egypt. All rights reserved<br/>        <span>All trademarks, registered trademarks and logos are the property of their respective owners</span>    </div></div></body></html>
--- application/views/backend/includes/header.php
+++ application/views/backend/includes/header.php
@@ -1,236 +1,99 @@
-<html>
-
-
-
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=10" />
+        <title>Administration - Intel ISEF Egypt</title>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/pure-min.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/site.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/base.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/accordion.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/breadcrumb.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/button.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/dropdown_slidingmenus.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/font.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/form.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/header.css" />	
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/icon.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/listview.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/pagination.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/progress_bar.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/radiobuttons_checkboxes.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/switch.css" />	
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/tab.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/table.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/title_bar.css" />	
+        <script src="<?php echo base_url(); ?>assets/frontend/js/jquery-1.10.1.js"></script>		
+        <!--[if lt IE 9]>		
+                <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/fallback.css" />
+                <script src="<?php echo base_url(); ?>assets/frontend/js/modernizr.js"></script>
+                <script src="<?php echo base_url(); ?>assets/frontend/js/respond.min.js"></script>
+                <script src="<?php echo base_url(); ?>assets/frontend/js/PIE.js"></script>			
+        <![endif]-->
+        <script src="<?php echo base_url(); ?>assets/frontend/js/master.js"></script>
 
-
-
-        <title>EL SHEIKH</title>
-
-
-
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-
-
-        <link type="text/css" href="<?php echo base_url(); ?>assets/backend/css/style.css" rel="stylesheet"/>       
-
-
-
-        <script type="text/javascript" src="<?php echo base_url(); ?>assets/backend/js/jquery/core/jquery6.min.js" ></script>
-
-
-
-         <script type="text/javascript" src="<?php echo base_url(); ?>assets/backend/js/common/common.js" ></script>
-
-
-
-         <style>
-
-
-
-             #navbar {
-
-
-
-	margin: 0;
-
-
-
-	padding: 0;
-
-
-
-	height: 1em; }
-
-
-
-#navbar li {
-
-
-
-	list-style: none;
-
-
-
-	float: left; }
-
-
-
-#navbar li a {
-
-
-
-	display: block;
-
-
-
-	padding: 3px 8px;
-
-
-
-	background-color: #4c4c4c;
-
-
-
-	color: #fff;
-
-
-
-	text-decoration: none; }
-
-
-
-#navbar li ul {
-
-
-
-	display: none;
-
-
-
-	width: 10em; /* Width to help Opera out */
-
-
-
-	background-color: #69f;
-
-
-
-	z-index:300;}
-
-
-
-#navbar li:hover ul {
-
-
-
-	display: block;
-
-
-
-	position: absolute;
-
-
-
-	margin: 0;
-
-
-
-	padding: 0; }
-
-
-
-#navbar li:hover li {
-
-
-
-	float: none; }
-
-
-
-#navbar li:hover li a {
-
-
-
-	background-color: #d20025;
-
-
-
-	border-bottom: 1px solid #fff;
-
-
-
-	border-top: 1px solid #fff;
-
-
-
-	color: #fff; }
-
-
-
-#navbar li li a:hover {
-
-
-
-	background-color: #4c4c4c; }
-
-
-
-         </style>
-
-
-
-        <meta http-equiv="Content-Type" content="text/html;charse"/>
     </head>
     <body>
-    <div class="header">
-    <div class="logo-header"></div>
-        <?php $user=$this->session->all_userdata();if(!empty ($user['username'])): ?> 
-        <span class="username">
-                <span class="welcome-name">Welcome:</span> 
-                <span class="user-name"><?php echo $user['username']?></span>
-                <span class="logout"> <?=anchor('home/logout','Logout')?></span>
-        </span>  
-    </div>
-
-<div class="menu"><span class="goto"> <ul id="navbar">
-
-	<li><a >Go To</a><ul>
-
-
-
-		<li><?=anchor('admin/home/backend','Home')?></li>
-
-
-
-		<li><?=anchor('admin/usermanager/home','Users')?></li>
-
-
-
-		<li><?=anchor('admin/contentcontroller/home','Content')?></li>
-
-
-
-<!--		<li><?=anchor('admin/category/home','Category')?></li>
-
-
-
-		<li><?=anchor('admin/offer/home','Offer')?></li>
-
-
-
-		<li><?=anchor('admin/product/home','Product')?></li>-->
-
-
-
-		<li><?=anchor('admin/configurationmanager/configurationform','Configuration')?></li>
-
-
-
+        <header class="pure-g-r intel-hdr">							
+            <div class="intel-u-1 intel-desktop" style="z-index: 10000">								
+                <div class="intel-hdr-box-wrap">
+                    <div class="intel-hdr-box-info-left">
+                        <div class="intel-hdr-infobar">
+                            <span class="intel-hdr-ww"><?php echo date("M d Y") ?></span>
+                            <span class="intel-hdr-user">Hello 
+                                <?php $user = $this->session->all_userdata(); ?>!
+                            </span>
+                            <script type="text/javascript">
+                                $(function() {
+                                    $(".intel-hdr").each(function() {
+                                        if (!($(this).attr('id'))) {
+                                            $(this).attr("id", "hdr" + IUtility.guid());
+                                        }
+                                        var tmpHeader = new IHeader("#" + $(this).attr("id"), null, null, null, null, null, "<?php echo $user['username'] ?>");
+                                        tmpHeader.init();
+                                    });
+                                });
+                            </script>
+                            <!--                            <a href="javascript:void(0);" class="intel-hdr-help">
+                                                            <span class="icon-b_help"> </span>
+                                                            Help
+                                                        </a>-->
+                            <!--                            <input type="search" id="iptSearch" name="iptSearch" placeholder="Search" />
+                                                        <a href="javascript:void(0);" class="intel-btn intel-btn-action intel-btn-icon-only intel-hdr-search">
+                                                            <span class="icon-search"> </span>
+                                                        </a>-->
+                        </div>
+                    </div>									
+                    <div class="intel-hdr-box-title">
+                        <div class="intel-hdr-title">Administration</div>
+                    </div>
+                </div>								
+                <div class="intel-hdr-box-logo intel-right intel-desktop">
+                    <a href="<?php echo base_url(); ?>"><img class="intel-hdr-logo" src="<?php echo base_url(); ?>assets/frontend/images/iesf.png"   alt="ISEF Logo"/></a>
+                </div>
+                <div class="social-icon"> <a href="#"><img src="<?php echo base_url(); ?>assets/frontend/images/mail-icon.png" width="24" height="25" /></a>
+                    <a href="#"><img src="<?php echo base_url(); ?>assets/frontend/images/youtube-icon.png" width="24" height="25" /></a>
+                    <a href="#"><img src="<?php echo base_url(); ?>assets/frontend/images/twitter-icon.png" width="24" height="25" /></a>
+                    <a href="#"><img src="<?php echo base_url(); ?>assets/frontend/images/facebook-icon.png" width="24" height="25" /></a>
+                </div>																
+            </div>
+            <div class="intel-u-1-4 intel-mobile">
+                <a href="javascript:void(0);" class="intel-hdr-btn-menu intel-left"><span class="icon-menu"> </span></a>
+            </div>
+            <div class="intel-u-1-2 intel-center intel-mobile">
+                <div class="intel-hdr-title">Administration</div>
+            </div>
+            <div class="intel-u-1-4 intel-right intel-mobile">
+                <a href="javascript:void(0);" class="intel-hdr-btn-next-simple ">Next <span class="icon-a_right"> </span></a>
+            </div>	
+        </header>
+        <div class="intel-tab" id="tab656864e7-cf97-c7d5-0f10-3f47c721042c" init="true">
+            <ul>
+                <li><a href="javascript:void(0);" tab="#tab1" class="active">Admin</a></li>
+                <li><a href="javascript:void(0);" tab="#tab2">Judges</a></li>
+                <li><a href="javascript:void(0);" tab="#tab3">Teams</a></li>
+                <li><a href="javascript:void(0);" tab="#tab4">Tasks</a></li>
+                <li><a href="javascript:void(0);" tab="#tab5">More...</a></li>
             </ul>
-
-
-
-	</li>
-
-
-
-</ul></span></div>
-
-
-
-
-
-
-
-<?php endif;?>
-
-
-
-
-
-
-
-        <br>
+            <hr class="intel-tab-divider">
+        </div>
