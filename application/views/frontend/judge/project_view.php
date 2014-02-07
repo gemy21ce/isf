f735130fc33bcba95a6e43d461e70dcf22383db9
Gamal Shaban <gemy21ce@gmail.com>
2014-02-07 16:57:55 +0200

    show project and evaluate project pages.

---
+++ application/views/frontend/judge/project_view.php
@@ -0,0 +1,20 @@
+<div class="intel-tab" id="tabs" init="true">
+    <ul style="margin-top: 10px;">
+        <li><a href="<?= base_url(); ?>judge/home" tab="#admins" class="active">المشاريع</a></li>
+        <li><a href="<?= base_url(); ?>judge/sched" tab="#sched">جدول التحكيم</a></li>
+        <!--<li><a href="javascript:void(0);" tab="#tab4">Tasks</a></li>-->
+        <!--<li><a href="javascript:void(0);" tab="#tab5">More...</a></li>-->
+    </ul>
+    <hr class="intel-tab-divider">
+</div>
+<article class="intel-tab-content">
+    <section class="active" id="teams">
+        <span class="Content-body">
+            <h2 id="Admin"><?= $project->name ?></h2>
+            <hr/>
+            <div class="contant-contaner">
+                p
+            </div>
+        </span>
+    </section>
+</article>
